0061a41feb1247ce275d2463c1b012a6a21b8cac
Lord QN <lordxxqn@Lordxqn.local>
2017-12-18 16:45:02 +0100

    Fizzbuzz solution with test cases

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,13 @@
 exports.fizzbuzz = function (number) {
-    switch (number) {
-        case 1: return number.toString();
-        case 3: return "FIZZ";
-        case 5: return "BUZZ";
-        default: return "";
+    const rules = [
+        [15, "FizzBuzz"],
+        [3, "FIZZ"],
+        [5, "BUZZ"]
+    ];
+    for (var i = 0; i < rules.length; i++) {
+        if (number % rules[i][0] === 0) {
+            return rules[i][1];
+        }
     }
+    return number.toString();
 };
--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -10,5 +10,22 @@ describe('Fizz Buzz tests', () => {
     it('Shoulrd return "BUZZ" if number 5 is given', () => {
         expect(fizzbuzz.fizzbuzz(5)).toEqual("BUZZ");
     });
-
+    it('Shoulrd return "FizzBuzz" if number 15 is given', () => {
+        expect(fizzbuzz.fizzbuzz(15)).toEqual("FizzBuzz");
+    });
+    it('Should return "FIZZ" if [number] is divisible by 3', () => {
+        expect(fizzbuzz.fizzbuzz(9)).toEqual("FIZZ");
+    });
+    it('Should return "BUZZ" if [number] is divisible by 5', () => {
+        expect(fizzbuzz.fizzbuzz(10)).toEqual("BUZZ");
+    });
+    it('Should return "BUZZ" if [number] is divisible by 5', () => {
+        expect(fizzbuzz.fizzbuzz(35)).toEqual("BUZZ");
+    });
+    it('Should return "FizzBuzz" if [number] is divisible by 3 and 5', () => {
+        expect(fizzbuzz.fizzbuzz(30)).toEqual("FizzBuzz");
+    });
+    it('Should return string when number given is not divisable by 3 nor 5', () => {
+        expect(fizzbuzz.fizzbuzz(8)).toEqual("8");
+    });
 });
