2c58b10cf255533f77c93ec1df64fe08c04a36e1
Lord QN <lordxxqn@Lordxqn.local>
2017-12-18 22:11:49 +0100

    Cleaning code

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -3,16 +3,9 @@
  * Function to return a string based on the number value
  */
 exports.fizzBuzz = function (number) {
-    const rules = [
-        [15, "FizzBuzz"],
-        [3, "FIZZ"],
-        [5, "BUZZ"]
-    ];
-    for (var i = 0; i < rules.length; i++) {
-        if (number % rules[i][0] === 0) {
-            return rules[i][1];
-        }
-    }
+    const rules = [[15, "FizzBuzz"], [3, "FIZZ"], [5, "BUZZ"]];
+    for (var i = 0; i < rules.length; i++)
+        if (number % rules[i][0] === 0) return rules[i][1];
     return number.toString();
 };
 
@@ -22,8 +15,6 @@ exports.fizzBuzz = function (number) {
 */
 exports.fizzBuzzGenerator = function (numbers) {
     var result = "";
-    for (var i = 0; i < numbers.length; i++) {
-        result += this.fizzBuzz(numbers[i]);
-    }
+    for (var i = 0; i < numbers.length; i++) result += this.fizzBuzz(numbers[i]);
     return result;
 };
